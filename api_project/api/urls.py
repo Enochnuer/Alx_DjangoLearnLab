1cbaa100ddccd56f620c9e3d320ef2ac47f20e5c
Enoch Nuertey <enochnuer111@gmail.com>
2024-09-01 19:32:45 +0000

    initial commit

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from .views import BookViewSet
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+
+router = DefaultRouter()
+router.register(r"book", BookViewSet)
 
 urlpatterns = [
-    path("Books/", BookList.as_view(), name="book_list" )
+    path("", include(router.urls) ),
+    path("api-token-auth/", obtain_auth_token, name="api-token-auth"),
 ]
---
+++ makefile
@@ -0,0 +1,12 @@
+run:
+    python manage.py makemigrations
+	python manage.py migrate
+	python manage.py runserver
+
+jit:
+    git add 
+	git commit -m "initial commit"
+	git push
+	
+
+    
