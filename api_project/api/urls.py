84c5afbee3ff93bc234856efa9b243ddfa43cabf
Enoch Nuertey <enochnuer111@gmail.com>
2024-09-14 21:22:29 +0000

    third commit

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
-router.register(r"book", BookViewSet)
+router.register(r"Book", BookViewSet)
 
 urlpatterns = [
     path("", include(router.urls) ),
